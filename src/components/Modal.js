0b3287c563753bcca7577f968bf9d90e17b29d10
Andre Foulem <timmycasual@icloud.com>
2022-09-16 11:24:21 -0700

    Modal: episodes

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import screen from '../assets/figma/screen1.png';
+import './Modal.scss';
 
 const MODAL_STYLES = {
   position: 'fixed',
-  top: '50%',
+  top: '40%',
   left: '50%',
   transform: 'translate(-50%,-50%)',
-  backgroundColor: '#fff',
-  padding: '50px',
   zIndex: 100,
-  width: '500px',
-  height: '500px',
+  width: '800px',
+  height: '1000px',
+  borderRadius: '40px',
+  backgroundColor: '#111111',
 };
 
 const OVERLAY_STYLES = {
@@ -30,8 +32,24 @@ export default function Modal({ open, children, onClose }) {
     <>
       <div style={OVERLAY_STYLES}></div>
       <div style={MODAL_STYLES}>
-        <button onClick={onClose}>Close Modal</button>
-        {children}
+        <div className="a_modal">
+          <div className="a_modal__header">
+            <img
+              className="a_modal__screen"
+              onClick={onClose}
+              src={screen}
+              alt=""
+            />
+          </div>
+          <div className="a_modal__bottomSection">
+            <div className="a_modal__episodes">
+              <p className="a_modal__episodesText">Episodes</p>
+            </div>
+            <hr className="a_modal__divider1"></hr>
+          </div>
+          {/* <button onClick={onClose}>Close Modal</button> */}
+          {/* {children} */}
+        </div>
       </div>
     </>,
     document.getElementById('portal')
