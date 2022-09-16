04414c7edd79c1452257b2bbc138b9e7ec05b36f
Andre Foulem <timmycasual@icloud.com>
2022-09-16 13:19:02 -0700

    asdf

--- src/components/Section4/Section4.js
+++ src/components/Section4/Section4.js
@@ -8,96 +8,99 @@ import Modal from '../Modal';
 import topTv from '../../data/top10tv';
 import { useState } from 'react';
 export default function Section4(props) {
-    const tvList = topTv();
-    const [topRightModal, setTopRightModal] = useState(false);
-    const { channel } = props;
+  const tvList = topTv();
+  const [topRightModal, setTopRightModal] = useState(false);
+  const { channel } = props;
 
-    const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-    const BUTTON_WRAPPER_STYLES = {
-        position: 'relative',
-        zIndex: 1,
-    };
+  const BUTTON_WRAPPER_STYLES = {
+    position: 'relative',
+    zIndex: 1,
+  };
 
-    const toggleShow = () => setTopRightModal(!topRightModal);
-    return (
-        <div className="k__section4">
-            <div className="k-s4__cover">
-                <Carousel>
-                    <Carousel.Item interval={5000}>
-                        <img className="k-s4__cover-img" src={image} alt="First slide" />
-                        <Carousel.Caption>
-                            <h3
-                                className="k-s4__onCLickModal"
-                                onClick={() => setIsOpen(true)}
-                            >
-                                First slide label
-                            </h3>
-                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item interval={5000}>
-                        <img className="k-s4__cover-img" src={image0} alt="Second slide" />
-                        <Carousel.Caption>
-                            <h3>Second slide label</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item interval={5000}>
-                        <img className="k-s4__cover-img" src={image} alt="Third slide" />
-                        <Carousel.Caption>
-                            <h3>Third slide label</h3>
-                            <p>
-                                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-                            </p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                </Carousel>
-                <Modal open={isOpen} onClose={() => setIsOpen(false)}>
-                    Fancy Modal
-                </Modal>
-                {/* <img className='k-s4__cover-img' src={image} /> */}
-            </div>
-            <p className="k-s4__title">Recommanded for you</p>
-            <div className="k-s4__img-wrapper">
-                {channel === "none" ? tvList.map(
-                    (e, i) =>
-                        i <= 5 && (
-                            <>
-                                <img
-                                    key={e.id}
-                                    className="k-s4__list-img"
-                                    src={e.image}
-                                    alt=""
-                                />
-                            </>
-                        )
-                ) : channel === "netflix" ? tvList.filter(e => e.channel === "netflix").map(
-                    (e, i) =>
-                        i <= 5 && (
-                            <>
-                                <img
-                                    key={e.id}
-                                    className="k-s4__list-img"
-                                    src={e.image}
-                                    alt=""
-                                />
-                            </>
-                        )
-                ) : tvList.filter(e => e.channel === "prime").map(
-                    (e, i) =>
-                        i <= 5 && (
-                            <>
-                                <img
-                                    key={e.id}
-                                    className="k-s4__list-img"
-                                    src={e.image}
-                                    alt=""
-                                />
-                            </>
-                        )
-                )}
-            </div>
-        </div>
-    );
+  const toggleShow = () => setTopRightModal(!topRightModal);
+  return (
+    <div className="k__section4">
+      <div className="k-s4__cover">
+        <Carousel onClick={() => setIsOpen(true)}>
+          <Carousel.Item interval={5000}>
+            <img className="k-s4__cover-img" src={image} alt="First slide" />
+            <Carousel.Caption>
+              <h3 className="k-s4__onCLickModal">First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item interval={5000}>
+            <img className="k-s4__cover-img" src={image0} alt="Second slide" />
+            <Carousel.Caption>
+              <h3>Second slide label</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item interval={5000}>
+            <img className="k-s4__cover-img" src={image} alt="Third slide" />
+            <Carousel.Caption>
+              <h3>Third slide label</h3>
+              <p>
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+        <Modal open={isOpen} onClose={() => setIsOpen(false)}>
+          Fancy Modal
+        </Modal>
+        {/* <img className='k-s4__cover-img' src={image} /> */}
+      </div>
+      <p className="k-s4__title">Recommanded for you</p>
+      <div className="k-s4__img-wrapper">
+        {channel === 'none'
+          ? tvList.map(
+              (e, i) =>
+                i <= 5 && (
+                  <>
+                    <img
+                      key={e.id}
+                      className="k-s4__list-img"
+                      src={e.image}
+                      alt=""
+                    />
+                  </>
+                )
+            )
+          : channel === 'netflix'
+          ? tvList
+              .filter((e) => e.channel === 'netflix')
+              .map(
+                (e, i) =>
+                  i <= 5 && (
+                    <>
+                      <img
+                        key={e.id}
+                        className="k-s4__list-img"
+                        src={e.image}
+                        alt=""
+                      />
+                    </>
+                  )
+              )
+          : tvList
+              .filter((e) => e.channel === 'prime')
+              .map(
+                (e, i) =>
+                  i <= 5 && (
+                    <>
+                      <img
+                        key={e.id}
+                        className="k-s4__list-img"
+                        src={e.image}
+                        alt=""
+                      />
+                    </>
+                  )
+              )}
+      </div>
+    </div>
+  );
 }
