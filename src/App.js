e3a87951fbd58218b96fb5b5647b9fdb9413dfd6
Andre Foulem <timmycasual@icloud.com>
2022-09-15 20:05:40 -0700

    Style app.scss

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import './App.scss';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello</header>
+      <section className="a_site">
+        <div className="a_site__flexcolumn">
+          <div className="a_site__columnLeft"></div>
+          <div className="a_site__columnRightBackground"></div>
+          <div className="a_site__columnRight"></div>
+        </div>
+      </section>
     </div>
   );
 }
---
+++ src/components/TopNav.js
@@ -0,0 +1,13 @@
+import './TopNav.scss';
+
+function App() {
+  return (
+    <div className="a_topnav">
+      <div className="a_topnav__flexrow">
+        <div className="a_topnav__Logo"></div>
+        <div className="a_topnav__InputBox"></div>
+      </div>
+    </div>
+  );
+}
+export default App;
