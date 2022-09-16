1b2a42dea211acc78cf6c40cf54a2b8843a7ebe7
Andre Foulem <timmycasual@icloud.com>
2022-09-15 17:14:39 -0700

    Top 10 images data + jpg

---
+++ src/data/top10movie.js
@@ -0,0 +1,102 @@
+export const movie = [
+  {
+    title: 'THOR: LOVE AND THUNDER',
+    description:
+      'Thor enlists the help of Valkyrie, Korg and ex-girlfriend Jane Foster to fight Gorr the God Butcher, who intends to make the gods extinct.',
+    genre: 'action',
+    ratings: '8.5',
+    channel: 'Disney',
+    id: 1,
+    img: 'thor.jpg',
+  },
+  {
+    title: 'Licorice Pizza',
+    description:
+      'It may not have won Best Picture, but cinephiles should be sure to check out Paul Thomas Anderson’s nostalgic coming-of-age drama, Licorice Pizza, if they haven’t already. It was a controversial nomination this awards season, both because it depicts a romance between a teenage boy and a 25-year-old woman, and because of two scenes that invite audiences to laugh at a Japanese woman’s expense. The latter is absolutely offensive and inexcusable, but the former is, in my humble opinion, handled with appropiate care. The authentic charm of Alana Haim and Cooper Hoffman—the late Phillip Seymour Hoffman’s son—will make you believe in love again, and Anderson’s skill with the camera will remind you why you love cinema. You may just want to fast-forward when you see John Michael Higgins on screen.',
+    genre: 'comedy',
+    ratings: '9',
+    channel: 'Amazon Prime',
+    id: 2,
+    image: 'licorice.jpg',
+  },
+  {
+    title: 'Elvis',
+    description:
+      'THE MAN. THE LEGEND. THE KING OF ROCK ROLL. The life story of Elvis Presley as seen through the complicated relationship with his enigmatic manager, Colonel Tom Parker.',
+    genre: 'Music',
+    ratings: '7',
+    channel: 'Crave',
+    id: 3,
+    image: 'elvis.jpg',
+  },
+  {
+    title: 'Flux Gourmet',
+    description:
+      'A dysfunctional sonic collective – a band devoted to the sounds of the culinary arts – navigates rivalries internal and external in this absurdly original feast for the senses. A SHUDDER EXCLUSIVE',
+    genre: 'Horror',
+    ratings: '6',
+    channel: 'Shudder',
+    id: 4,
+    image: 'fluxgourmet.jpg',
+  },
+  {
+    title: 'Everything Everywhere All At Once',
+    description:
+      'An aging Chinese immigrant is swept up in an insane adventure, in which she alone can save the world by exploring other universes connecting with the lives she could have led',
+    genre: 'Comedy',
+    ratings: '6',
+    channel: 'Apple TV',
+    id: 5,
+    image: 'everything.jgp',
+  },
+  {
+    title: 'Day Shift',
+    description:
+      'A hard-working, blue-collar dad just wants to provide a good life for his quick-witted 10-year-old daughter. His mundane San Fernando Valley pool cleaning job is a front for his real source of income: hunting and killing vampires.',
+    genre: 'Action',
+    ratings: '7',
+    channel: 'Netflix',
+    id: 6,
+    image: 'dayshift.jpg',
+  },
+  {
+    title: 'District 9',
+    description:
+      'An extraterrestrial race forced to live in slum-like conditions on Earth suddenly finds a kindred spirit in a government agent who is exposed to their biotechnology.',
+    genre: 'Science Fiction',
+    ratings: '7',
+    channel: 'SyFy',
+    id: 7,
+    image: 'district.jpg',
+  },
+  {
+    title: 'Minion',
+    description:
+      "The untold story of one twelve-year-old's dream to become the world's greatest supervillain.",
+    genre: 'Family',
+    ratings: '9',
+    channel: 'Disney',
+    id: 8,
+    image: 'minion',
+  },
+  {
+    title: 'About Wedding of a Lifetime',
+    description:
+      "A recently separated couple rekindles their romance when they are inadvertently entered into a nationally televised contest competing for an all-expenses-paid for wedding. Starring Jonathan Bennett and Brooke D'Orsay.",
+    genre: 'Romantic Comedy',
+    ratings: '8',
+    channel: 'Hallmark Channel',
+    id: 9,
+    image: 'wedding.jpg',
+  },
+  {
+    title: 'Belfast',
+    description:
+      'Written and directed by Academy Award® nominee Kenneth Branagh. this drama is a poignant story of love, laughter and loss in one boy’s childhood, amid the music and social tumult of the late 1960s. Starring Caitríona Balfe, Judi Dench, Jamie Dornan, Ciaran Hinds, and Jude Hill.',
+    genre: 'Drama',
+    ratings: '9',
+    channel: 'Crave',
+    id: 10,
+    image: 'belfast',
+  },
+];
