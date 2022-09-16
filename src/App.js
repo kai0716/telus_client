f1f3529bb67ac9a1b166b90b1e5dc081e3dc1294
Andre Foulem <timmycasual@icloud.com>
2022-09-15 20:42:16 -0700

    top nav

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.scss';
+import TopNav from './components/TopNav';
 
 function App() {
   return (
     <div className="App">
       <section className="a_site">
         <div className="a_site__flexcolumn">
+          <div className="a_site__columnLeftBackground"></div>
           <div className="a_site__columnLeft"></div>
           <div className="a_site__columnRightBackground"></div>
-          <div className="a_site__columnRight"></div>
+          <div className="a_site__columnRight">
+            <TopNav />
+          </div>
         </div>
       </section>
     </div>
--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -1,11 +1,17 @@
 import './TopNav.scss';
+import logo from '../assets/figma/logo.png';
 
 function App() {
   return (
     <div className="a_topnav">
       <div className="a_topnav__flexrow">
-        <div className="a_topnav__Logo"></div>
-        <div className="a_topnav__InputBox"></div>
+        <img className="a_topnav__logo" src={logo} alt="logo" />
+
+        <input
+          className="a_topnav__Input"
+          type="input"
+          placeholder="What do you want to watch?"
+        />
       </div>
     </div>
   );
