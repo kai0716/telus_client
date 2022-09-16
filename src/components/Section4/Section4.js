1c8c567fc3b0110a8ea25fd65f595c3294895f74
kai0716 <kevin-zhang98@hotmail.com>
2022-09-16 10:44:56 -0700

    change image format

--- src/components/Section4/Section4.js
+++ src/components/Section4/Section4.js
@@ -1,5 +1,5 @@
-import image from '../../assets/images/image9.jpg';
-import image0 from '../../assets/images/image0.jpg';
+import image from '../../assets/images/image1.png';
+import image0 from '../../assets/images/image0.png';
 import './Section4.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Carousel from 'react-bootstrap/Carousel';
@@ -7,56 +7,56 @@ import Carousel from 'react-bootstrap/Carousel';
 import topTv from '../../data/top10tv';
 import { useState } from 'react';
 export default function Section4(props) {
-  const tvList = topTv();
-  const [topRightModal, setTopRightModal] = useState(false);
+    const tvList = topTv();
+    const [topRightModal, setTopRightModal] = useState(false);
 
-  const toggleShow = () => setTopRightModal(!topRightModal);
-  return (
-    <div className="k__section4">
-      <div className="k-s4__cover">
-        <Carousel>
-          <Carousel.Item interval={5000}>
-            <img className="k-s4__cover-img" src={image0} alt="First slide" />
-            <Carousel.Caption>
-              <h3>First slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item interval={5000}>
-            <img className="k-s4__cover-img" src={image} alt="Second slide" />
-            <Carousel.Caption>
-              <h3>Second slide label</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item interval={5000}>
-            <img className="k-s4__cover-img" src={image} alt="Third slide" />
-            <Carousel.Caption>
-              <h3>Third slide label</h3>
-              <p>
-                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-              </p>
-            </Carousel.Caption>
-          </Carousel.Item>
-        </Carousel>
-        {/* <img className='k-s4__cover-img' src={image} /> */}
-      </div>
-      <p className="k-s4__title">Recommanded for you</p>
-      <div className="k-s4__img-wrapper">
-        {tvList.map(
-          (e, i) =>
-            i <= 5 && (
-              <>
-                <img
-                  key={e.id}
-                  className="k-s4__list-img"
-                  src={e.image}
-                  alt=""
-                />
-              </>
-            )
-        )}
-      </div>
-    </div>
-  );
+    const toggleShow = () => setTopRightModal(!topRightModal);
+    return (
+        <div className="k__section4">
+            <div className="k-s4__cover">
+                <Carousel>
+                    <Carousel.Item interval={5000}>
+                        <img className="k-s4__cover-img" src={image0} alt="First slide" />
+                        <Carousel.Caption>
+                            <h3>First slide label</h3>
+                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item interval={5000}>
+                        <img className="k-s4__cover-img" src={image} alt="Second slide" />
+                        <Carousel.Caption>
+                            <h3>Second slide label</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item interval={5000}>
+                        <img className="k-s4__cover-img" src={image} alt="Third slide" />
+                        <Carousel.Caption>
+                            <h3>Third slide label</h3>
+                            <p>
+                                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                            </p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                </Carousel>
+                {/* <img className='k-s4__cover-img' src={image} /> */}
+            </div>
+            <p className="k-s4__title">Recommanded for you</p>
+            <div className="k-s4__img-wrapper">
+                {tvList.map(
+                    (e, i) =>
+                        i <= 5 && (
+                            <>
+                                <img
+                                    key={e.id}
+                                    className="k-s4__list-img"
+                                    src={e.image}
+                                    alt=""
+                                />
+                            </>
+                        )
+                )}
+            </div>
+        </div>
+    );
 }
